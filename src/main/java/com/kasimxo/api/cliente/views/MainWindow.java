bc4675e6f75587ecc3ee037443bf641b74eb2069
kasimxo <kasimxo@gmail.com>
2024-02-26 10:20:31 +0100

    Added controller for actions on ui

--- src/main/java/com/kasimxo/api/cliente/views/MainWindow.java
+++ src/main/java/com/kasimxo/api/cliente/views/MainWindow.java
@@ -2,7 +2,6 @@ package com.kasimxo.api.cliente.views;
 
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.io.File;
 
 import javafx.application.Application;
@@ -16,29 +15,17 @@ public class MainWindow extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		System.out.println("nepe");
 		try {
 			File r = new File("./src/main/java/com/kasimxo/api/cliente/views/fxml/mainwindow.fxml");
 			Parent root = FXMLLoader.load(r.toURL());
-            //Parent root = FXMLLoader.load(getClass().getResource("./src/main/java/com/kasimxo/api/cliente/views/fxml/mainwindow.fxml"));
             Scene scene = new Scene(root);
+            
             primaryStage.setScene(scene);
             primaryStage.show();
        } catch(Exception e) {
             e.printStackTrace();
        } 
-		
-		/*
-		//Create Stage
-		Stage newWindow = new Stage();
-		newWindow.setTitle("New Scene");
-		//Create view from FXML
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("mainwindow.fxml"));
-		//Set view in window
-	
-		newWindow.setScene(new Scene(loader.load()));
-		newWindow.show();
-		*/
+
 	}
 	
 	
---
+++ src/main/java/com/kasimxo/api/cliente/views/controller/Cotroller.java
@@ -0,0 +1,14 @@
+package com.kasimxo.api.cliente.views.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Cotroller {
+
+	@FXML
+	public void seleccionarArchivo(ActionEvent event) {
+		System.out.println("Vamos a seleccionar un archivo");
+	}
+	
+	
+}
